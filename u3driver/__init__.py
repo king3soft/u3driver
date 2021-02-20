889c447d3fd0e9d044c4c4958c1e82a4c27bc2b9
LFasd <2817950025@qq.com>
2021-02-20 17:07:06 +0800

    Update __init__.py

--- u3driver/__init__.py
+++ u3driver/__init__.py
@@ -1,4 +1,4 @@
-from u3driver.commands import *
+from u3driver.commands import * # 这个顺序必须排第一，如果让 AltElement 放第一会报错
 from u3driver.altElement import AltElement
 from u3driver.by import By
 from u3driver.runner import *
